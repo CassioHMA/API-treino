a3831bc5e8c368ba6c22c77ac7d6b8d6abab5b8e
cassioHMA <cassiohemachado@gmail.com>
2026-03-03 20:22:34 -0300

    tratando os erros

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,9 +2,9 @@ import { PrismaPg } from "@prisma/adapter-pg";
 
 import { PrismaClient } from "../generated/prisma/client.js";
 
-const conectionString = `${process.env.DATABASE_URL}`;
+const connectionString = `${process.env.DATABASE_URL}`;
 
-const adapter = new PrismaPg(conectionString);
+const adapter = new PrismaPg({ connectionString });
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
