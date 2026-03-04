eacd69b8288947c60687a59885140a4630d9bc1a
cassioHMA <cassiohemachado@gmail.com>
2026-03-04 15:53:14 -0300

    feat: add workoout session

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import "dotenv/config";
 import fastifyCors from "@fastify/cors";
 import fastifySwagger from "@fastify/swagger";
 import fastifyApiReference from "@scalar/fastify-api-reference";
-import { fromNodeHeaders } from "better-auth/node";
 import Fastify from "fastify";
 import {
   jsonSchemaTransform,
@@ -12,10 +11,9 @@ import {
   ZodTypeProvider,
 } from "fastify-type-provider-zod";
 
-import { NotFoundError } from "./errors/index.js";
 import { auth } from "./lib/auth.js";
-import { ErrorSchema, workoutPlanSchema } from "./schemas/index.js";
-import { CreateWorkoutPlan } from "./usecases/CreateWorkoutPlan.js";
+import workoutPlanRoutes from "./routes/workout-pla.js";
+
 const app = Fastify({
   logger: true,
 });
@@ -62,55 +60,10 @@ await app.register(fastifyApiReference, {
   },
 });
 
-app.withTypeProvider<ZodTypeProvider>().route({
-  method: "POST",
-  url: "/workouts-plans",
-  schema: {
-    body: workoutPlanSchema.omit({ id: true }),
-    response: {
-    201: workoutPlanSchema,
-    400: ErrorSchema,
-    401: ErrorSchema,
-    404: ErrorSchema,
-    500: ErrorSchema,
-    },
-  },
-  handler: async (request, reply) => {
-    try {
-      const session = await auth.api.getSession({
-        headers: fromNodeHeaders(request.headers)
-      })
-      if (!session) {
-        return reply.status(401).send({
-          error: "Unauthorized",
-          code: "UNAUTHORIZED",
-        });
-  
-      }
-      const createWorkoutPlan = new CreateWorkoutPlan();
-      const result = await createWorkoutPlan.execute({
-        userId: session.user.id,
-        name: request.body.name,
-        workoutDays: request.body.workoutDays,
-      });
-  
-    return reply.status(201).send(result)
-  } catch (error) {
-    app.log.error(error);
-    if (error instanceof NotFoundError){
-      return reply.status(404).send({
-        error: error.message,
-        code: "NOT_FOUND_ERROR",
-      })
-    }
-    return reply.status(500).send({
-      error: "Internal server error",
-      code: "INTERNAL_SERVER_ERROR",
-    });
-  }
-},
-});
+//routes
+await app.register(workoutPlanRoutes, { prefix: "/workout-plans" });
 
+//cotroller
 app.withTypeProvider<ZodTypeProvider>().route({
   method: "GET",
   url: "/swagger.json",
